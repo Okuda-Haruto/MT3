99e953da6f063456533ef42cbbd4cec2d6f9fe2f
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2025-04-11 12:29:28 +0900

    00_02確認課題

---
+++ Matrix4x4_operation.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include<Matrix4x4.h>
+
+//行列の加法
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
+//行列の減法
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
+//行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+//逆行列
+Matrix4x4 Inverse(const Matrix4x4& m);
+//転置行列
+Matrix4x4 Transpose(const Matrix4x4& m);
+//単位表列の作成
+Matrix4x4 MakeIdentity4x4();
